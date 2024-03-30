9e7ae9ed816e42f74199e96dd7645f61aaa2064e
ketan1435 <shendkarketan1435@gmail.com>
2024-03-30 14:01:29 +0530

    first day

---
+++ lib/presentation/utils/app_colors.dart
@@ -0,0 +1,13 @@
+
+import 'dart:ui';
+
+class AppColor
+{
+  static const headerTextColor = Color(0xff232323);
+  static const textColor1 = Color(0xff000000);
+  static const textColor2 = Color(0xffffffff);
+  static const backGroundColor = Color(0xffD9D9D9);
+  static const buttonGroundColor = Color(0xff1D60AE);
+  static const viewTextColor = Color(0xff6791FF);
+  static const dilougeBackGroundColor = Color(0xffDDECFC);
+}
